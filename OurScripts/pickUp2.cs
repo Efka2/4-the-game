d7a7883f8a6a232149378fdcca998fad59e3e10b
Evald Šilobrit <61195291+Efka2@users.noreply.github.com>
2020-05-31 23:23:59 +0300

    Update pickUp2.cs
    Deleted some comments

--- OurScripts/pickUp2.cs
+++ OurScripts/pickUp2.cs
@@ -12,11 +12,7 @@ public class pickUp2 : MonoBehaviour
     private Color matColor;
     public float pickUpDistance = 3;
     public AudioSource pageSound;
-    //public bool inRange = false;
-    //darbas su display'u lapeliu teksto
-    //public Text myText;
-    //public float fadeTime;
-    //public string myString;
+    
     public playerControler pc;
 
 
@@ -69,27 +65,13 @@ public class pickUp2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //myText = GameObject.Find("Text").GetComponent<Text>();
-        //myText.color = Color.clear;
+        
     }
 
-    //void FadeText()
-    //{
-    //    if (isHighlighted)
-    //    {
-    //        myText.text = myString;
-    //        myText.color = Color.Lerp(myText.color, Color.white, fadeTime * Time.deltaTime);
-    //    }
-    //    else
-    //    {
-    //        myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
-    //    }
-    //}
-
+    
 
     public GameObject TeleportTo;
     public GameObject TeleportFrom;
-    //private GameObject Player;
     static bool noRotateView = false;
     public float applyRotationX = 0.0f;
     public float applyRotationY = 0.0f;
@@ -99,73 +81,33 @@ public class pickUp2 : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        //speed = speed * -1;
-        //colorPicker = Random.Range(0, 10);
-       
-            try
-            {
+        
                 CharacterController cc = other.GetComponent<CharacterController>();
                 Transform tr = other.GetComponent<Transform>();
                 cc.enabled = false;
 
-                //object fpc = other.GetComponent<FirstPersonController>();
-                //GameObject.Find("Player").GetComponent("First Person Controller").enabled = false;
-                //Player.GetComponent("First Person Controller").enabled = false;
-                //var asd = GameObject.Find("Player").GetComponent("ridgidbody");
-                //other.enabled = false;
-
-                //asd.enabled = false;
-
-                //Player.transform.Rotate(0, 30f, 0, Space.World);
-                //other.transform.Rotate(90.0f, 50.0f, 30.0f, Space.Self);
-                //other.transform.rotation = new Quaternion(90.0f, 50.0f, 30.0f, 30.0f);
-
-                //other.transform.rotation = TeleportTo.transform.rotation;
-
-                //other.transform.rotation = Quaternion.Euler(
-                //    TeleportTo.transform.rotation.x + other.transform.rotation.x,
-                //    TeleportTo.transform.rotation.y + other.transform.rotation.y,
-                //    TeleportTo.transform.rotation.z + other.transform.rotation.z);
-
-                /*other.transform.rotation = Quaternion.Euler(
-                    applyRotationX + tr.rotation.x,
-                    applyRotationY + tr.rotation.y,
-                    applyRotationZ + tr.rotation.z);*/
+               
 
                 other.transform.Rotate(applyRotationX, applyRotationY, applyRotationZ, Space.Self);
 
                 other.GetComponent<FirstPersonController>().mouseReset();
 
-                //other.GetComponent(FirstPersonController).mouseReset();
-
-                //other.transform.position = new Vector3(
-                //    other.transform.position.x + TeleportTo.transform.position.x - this.transform.position.x,
-                //    other.transform.position.y + TeleportTo.transform.position.y - this.transform.position.y,
-                //    other.transform.position.z + TeleportTo.transform.position.z - this.transform.position.z);
+                
 
                 other.transform.position = new Vector3(
                     other.transform.position.x + TeleportTo.transform.position.x - this.transform.position.x,
                     other.transform.position.y + TeleportTo.transform.position.y - this.transform.position.y,
                     other.transform.position.z + TeleportTo.transform.position.z - this.transform.position.z);
 
-                //Camera.main.transform.rotation = Quaternion.Euler(TeleportTo.transform.rotation.x, TeleportTo.transform.rotation.y, TeleportTo.transform.rotation.z);
+               
                 cc.enabled = true;
                 if (FlickerFlashlight)
                 {
                     Flashlight asd = other.GetComponentInChildren<Flashlight>();
-                    //asd.flicker = true;
                     asd.flicker();
                 }
-                //other.enabled = true;
-                //other.transform.Rotate(TeleportTo.transform.rotation.x, TeleportTo.transform.rotation.y, TeleportTo.transform.rotation.z, Space.Self);
-            }
-            catch (System.Exception)
-            {
-                //System.Console.WriteLine(Exception);
-                //throw;
-            }
-
-            //other.transform.rotation = new Quaternion(0, 10, 0, 0);
+            
+            
         }
     
 
